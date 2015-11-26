71c955bb8b39fe27706e1e153e6ccd4ba1f83b63
bolans01 <bolans01@gmail.com>
2015-11-26 04:49:57 -0500

    Completed chap 5

--- ObjectiveCDemo/ViewController.m
+++ ObjectiveCDemo/ViewController.m
@@ -31,8 +31,8 @@
 	// Do any additional setup after loading the view.
     
 //CHAPTER 4::::
-//    NSString *myString = @"Objective-C";  //Created an Object from NextStep NSString class.
-//    self.lblDemo.text = [myString stringByAppendingString:@" is for me"];//myString is the object we are passing the message to, stringByAppendingString is the method we are calling.  @"..." is the string argument we are passing into the method.  The method will then output/return a string of type NSString and will be assigned to self.lblDemo.text.
+    NSString *myString = @"Objective-C";  //Created an Object from NextStep NSString class.
+    self.lblDemo.text = [myString stringByAppendingString:@" is for me"];//myString is the object we are passing the message to, stringByAppendingString is the method we are calling.  @"..." is the string argument we are passing into the method.  The method will then output/return a string of type NSString and will be assigned to self.lblDemo.text.
 //    NSUInteger stringLength = [myString length]; // In this method call, we must store the return into a variable called stringLength that must be of type NSUInteger.  We can use the 'length' property since it is inherited from NSString superclass.
 //    BOOL isEqualTo = [myString isEqualToString:@"Object-C"]; //With this method call we use the isEqualToString method created by Apple in the inherited NSString superclass.  The storing variable from the return must be explicitly defined as a BOOL.
 //    NSURL *myURL = [NSURL URLWithString:@"www.apple.com"];  //This method call, calls from the URLWithString class method from the NSURL class and returns a string of type NSURL.
@@ -41,7 +41,10 @@
 //CHAPTER 5:::::
     UITextField *textField = [[UITextField alloc]init]; //Creates an object reference from the pointer textField, object will be created in memory as located by pointer.  Alloc will send a message to the UITextField class to allocate memory space for the object.
 //This message call will pass the message init to the textField object and initialize all the variables.
-    textField.placeholder = @"First Name";
+    textField.placeholder = @"First Name"; //Accessing object properties with dot notation
+    textField.frame = CGRectMake(20, 70, 280, 31);  //Function CGRectMake will return rectangle and assign to frame property.
+    textField.borderStyle = UITextBorderStyleRoundedRect; //Set borderStyle property to UITextBorderStyleRoundedRect constant from UITextBorderStyle enumeration. 
+    [self.view addSubview:textField]; 
     
     
     
